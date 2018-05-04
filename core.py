02a1c2763eac2a21047e24b00ca03e6fd9f51651
Karina Gonzalez <94k.gonzalez@gmail.com>
2018-05-04 10:45:40 +0200

    position noise wrong

--- core.py
+++ core.py
@@ -14,17 +14,20 @@ N_COLUMNS = len(COLUMN_MAPPING)
 N_PARTICLES = 110
 LATTICE_WIDTH = 10
 LATTICE_LENGTH = int(N_PARTICLES / LATTICE_WIDTH)
-LATTICE_CONSTANT = 10
+LATTICE_CONSTANT = 2
 assert LATTICE_LENGTH * LATTICE_WIDTH == N_PARTICLES
 assert LATTICE_LENGTH > LATTICE_WIDTH
 
 MEAN_RADUIS = 1
 STD_RADIUS = 1 / 10
+seed = np.random
 
 def initialize_system():
+    """Initializes the system in a rectangle lattice with particles 
+        slightly deviated from the exact lattice positions"""
     system = np.zeros((N_PARTICLES, N_COLUMNS))
-    length_positions = np.arange(LATTICE_LENGTH) * LATTICE_CONSTANT
-    width_positions = np.arange(LATTICE_WIDTH) * LATTICE_CONSTANT
+    length_positions = np.arange(LATTICE_LENGTH) * LATTICE_CONSTANT + (np.random.rand() / STD_RADIUS)
+    width_positions = np.arange(LATTICE_WIDTH) * LATTICE_CONSTANT + (np.random.rand() / STD_RADIUS)
     lattice_x, lattice_y = np.meshgrid(length_positions, width_positions)
     # initialize positions
     system[:, COLUMN_REVERSE_MAPPING['x']] = lattice_x.flatten()
@@ -46,7 +49,7 @@ def plot_system(system):
     for x, y, r in zip(system[:, COLUMN_REVERSE_MAPPING['x']],
                        system[:, COLUMN_REVERSE_MAPPING['y']],
                        system[:, COLUMN_REVERSE_MAPPING['r']]):
-        print(x, y, r)
+        #print('x',x,'y', y,'r', r)
         circle = plt.Circle((x, y), radius=r)
         ax.add_artist(circle)
 
